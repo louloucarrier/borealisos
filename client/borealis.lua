4b78c5da3e3a1057404ca03def6d6c9cae3c5d63
louloucarrier <loulou.carrier26@gmail.com>
2026-08-21 13:50:50 +0200

    f
    f

---
+++ client/borealis.lua
@@ -0,0 +1,49 @@
+local basalt = require("basalt")
+
+local monitor = peripheral.find("monitor")
+
+if not monitor then
+    error("Aucun monitor détecté !")
+end
+
+local frame = basalt.createFrame()
+frame:setTerm(monitor)
+
+frame:setBackground(colors.black)
+
+frame:addLabel()
+    :setText("BOREALIS")
+    :setPosition(2, 2)
+    :setForeground(colors.white)
+
+frame:addLabel()
+    :setText("CENTRE DE CONTROLE")
+    :setPosition(2, 3)
+    :setForeground(colors.lightGray)
+
+frame:addLabel()
+    :setText("● SERVEUR ONLINE")
+    :setPosition(2, 5)
+    :setForeground(colors.lime)
+
+local tasks = frame:addButton()
+    :setText("TACHES")
+    :setPosition(2, 8)
+    :setSize(15, 3)
+
+local employees = frame:addButton()
+    :setText("EMPLOYES")
+    :setPosition(20, 8)
+    :setSize(15, 3)
+
+local reports = frame:addButton()
+    :setText("RAPPORTS")
+    :setPosition(2, 12)
+    :setSize(15, 3)
+
+local music = frame:addButton()
+    :setText("MUSIQUE")
+    :setPosition(20, 12)
+    :setSize(15, 3)
+
+basalt.autoUpdate()
